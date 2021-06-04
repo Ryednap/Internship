7ace93dce5aabd777c95a7b0af0679384df5bc9d
Ryednap <ujjwalpandey408@gmail.com>
2021-06-04 19:44:38 +0530

    First commit on the repo

---
+++ lib/setting2.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+final String appName = 'MCEC';
+final String description = "Delivery App";
+final String screenTitle = "Sign In to continue";
+
+/**
+ * Styling for Hint text in TextFormField
+ */
+
+final kHintTextStyle = TextStyle(
+  color: Colors.white,
+  fontFamily: 'OpenSans',
+
+);
+
+/**
+ * Label Style for User Typed Text in TextFormField
+ */
+
+final klabelStyle = TextStyle(
+  color: Colors.blue.shade600,
+  fontWeight: FontWeight.bold,
+  fontFamily: 'OpenSans',
+);
+
+/**
+ * Error Message for TexFormField Styling
+ */
+
+final kErrorStyle = TextStyle(
+  color : Colors.blue.shade600,
+  fontSize : 12.0,
+  fontStyle :FontStyle.italic,
+  fontWeight : FontWeight.w700,
+              
+);
+
+/**
+ * TextFormField Box Style
+ */
+
+final kBoxDecorationStyle = BoxDecoration(
+  color: Colors.pinkAccent.shade100,
+  borderRadius: BorderRadius.circular(9.0),
+  boxShadow: [
+    BoxShadow(
+      color: Colors.white,
+      blurRadius: 3.0,
+      offset: Offset(0, 2),
+    ),
+  ],
+);
+
+/**
+ * TextFormFiled Border Styling
+ */
+
+final kBorderStyle = OutlineInputBorder(
+    borderSide : BorderSide (color: Colors.deepPurple, width : 2.0),
+    borderRadius: BorderRadius.circular(10.0),
+    
+);
